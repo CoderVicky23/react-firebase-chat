1a531a5076f17cf48307db9ffefa2711212d7716
vicky singh <23vickysingh@gmail.com>
2024-10-11 18:05:20 +0530

    list created

--- src/components/chat/Chat.jsx
+++ src/components/chat/Chat.jsx
@@ -1,8 +1,99 @@
+import { useState } from "react"
 import "./chat.css"
+import EmojiPicker from "emoji-picker-react"
 
 const Chat = () => {
+
+    const [ open, setOpen ] = useState(false);
+    const [ text, setText ] = useState("");
+
+    const handleEmoji = (e) => {
+        setText(prev => prev + e.emoji);
+        setOpen(false);
+    }
     return (
-        <div className="chat">Chat</div>
+        <div className="chat">
+
+            <div className="top">
+                <div className="user">
+                    <img src="./public/avatar.png" alt="" />
+                    <div className="texts">
+                        <span>Jane Doe</span>
+                        <p>Lorem ipsum dolor sit, amet</p>
+                    </div>
+                </div>
+                <div className="icons">
+                    <img src="./public/phone.png" alt="" />
+                    <img src="./public/video.png" alt="" />
+                    <img src="./public/info.png" alt="" />
+                </div>
+            </div>
+
+            <div className="center">
+                <div className="message">
+                    <img src="./public/avatar.png" alt="" />
+                    <div className="texts">
+                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ratione molestias sequi hic? Tempora assumenda maiores soluta totam, aliquam iste ex.</p>
+                        <span>1 min ago</span>
+                    </div>
+                </div>
+                <div className="message own">
+                    <div className="texts">
+                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ratione molestias sequi hic? Tempora assumenda maiores soluta totam, aliquam iste ex.</p>
+                        <span>1 min ago</span>
+                    </div>
+                </div>
+                <div className="message">
+                    <img src="./public/avatar.png" alt="" />
+                    <div className="texts">
+                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ratione molestias sequi hic? Tempora assumenda maiores soluta totam, aliquam iste ex.</p>
+                        <span>1 min ago</span>
+                    </div>
+                </div>
+                <div className="message own">
+                    <div className="texts">
+                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ratione molestias sequi hic? Tempora assumenda maiores soluta totam, aliquam iste ex.</p>
+                        <span>1 min ago</span>
+                    </div>
+                </div>
+                <div className="message">
+                    <img src="./public/avatar.png" alt="" />
+                    <div className="texts">
+                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ratione molestias sequi hic? Tempora assumenda maiores soluta totam, aliquam iste ex.</p>
+                        <span>1 min ago</span>
+                    </div>
+                </div>
+                <div className="message own">
+                    <div className="texts">
+                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ratione molestias sequi hic? Tempora assumenda maiores soluta totam, aliquam iste ex.</p>
+                        <span>1 min ago</span>
+                    </div>
+                </div>
+                <div className="message">
+                    <img src="./public/avatar.png" alt="" />
+                    <div className="texts">
+                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ratione molestias sequi hic? Tempora assumenda maiores soluta totam, aliquam iste ex.</p>
+                        <span>1 min ago</span>
+                    </div>
+                </div>
+            </div>
+
+            <div className="bottom">
+                <div className="icons">
+                    <img src="./public/img.png" alt="" />
+                    <img src="./public/camera.png" alt="" />
+                    <img src="./public/mic.png" alt="" />
+                </div>
+                <input type="text" placeholder="Type a message ..." value={text} onChange={(e) => setText(e.target.value)} />
+                <div className="emoji">
+                    <img src="./public/emoji.png" alt="" onClick={() => setOpen(prev => !prev)} />
+                    <div className="picker">
+                        <EmojiPicker open={open} onEmojiClick={handleEmoji}/>
+                    </div>
+                </div>
+                <button className="sendButton">Send</button>
+            </div>
+        </div>
     )
 }
 
--- src/components/list/chatList/Chatlist.jsx
+++ src/components/list/chatList/Chatlist.jsx
@@ -1,8 +1,39 @@
+import { useState } from "react"
 import "./chatlist.css"
 
 const Chatlist = () => {
+    const [addMode, setAddMode] = useState(false)
     return (
-        <div className="userinfo">userinfo</div>
+        <div className="chatlist">
+            <div className="search">
+                <div className="searchBar">
+                    <img src="/public/search.png" alt="" />
+                    <input type="text" placeholder="Search" />
+                </div>
+                <img src={addMode ? "./public/minus.png" : "./public/plus.png"} alt="" className="add" onClick={() => setAddMode((prev) => !prev)}/>
+            </div>
+            <div className="item">
+                <img src="./public/avatar.png" alt="" />
+                <div className="texts">
+                    <span>Jane Doe</span>
+                    <p>Hello</p>
+                </div>
+            </div>
+            <div className="item">
+                <img src="./public/avatar.png" alt="" />
+                <div className="texts">
+                    <span>Jane Doe</span>
+                    <p>Hello</p>
+                </div>
+            </div>
+            <div className="item">
+                <img src="./public/avatar.png" alt="" />
+                <div className="texts">
+                    <span>Jane Doe</span>
+                    <p>Hello</p>
+                </div>
+            </div>
+        </div>
     )
 }
 
