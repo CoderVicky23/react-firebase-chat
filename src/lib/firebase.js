9658b4847238c0ad940335a1aa8b35d9c2e86321
vicky singh <23vickysingh@gmail.com>
2024-10-29 21:33:31 +0530

    firebase.js file created

---
+++ src/lib/firebase.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: import.meta.env.VITE_API_KEY,
+  authDomain: "reactchat-3fa2c.firebaseapp.com",
+  projectId: "reactchat-3fa2c",
+  storageBucket: "reactchat-3fa2c.appspot.com",
+  messagingSenderId: "815046498884",
+  appId: "1:815046498884:web:6ab5f25f1385888b3ffa11"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
